62cf50466f08f9238de0005bf64991036fc2f85c
Student <student@d17-327e-pc-07.ipa.ii.agh.edu.pl>
2025-01-12 10:52:21 +0100

    added logic condition to checheck length text

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useState} from "react";
+
 function App() {
-  let title = 'Wall-E';
+  // let title = 'Wall-E';
+  const [title, setTitile] = useState('Wall-E');
+
+  let msg = "" 
+
+    if (title.length<5)
+    {
+      msg = "title is to short"
+    }
+    else if(title.length<15)
+    {
+      msg = "title is OK"
+    }
+    else
+    {
+      msg = "title is to long" 
+    }
 
   function handleChange(event)
   {
-    console.log(event.target.value);
+    setTitile(event.target.value);
   }
+
 
   return (
     <div>
       <h1>My favourite movies to watch</h1>
       <h2>My favourite movies for today is {title}</h2>
-      <input type="text" onChange={handleChange}/>
+      {title.length > 0 && <div>{msg}</div>}
+      <input type="text" value={title} onChange={handleChange}/>
     </div>
   );
 }
